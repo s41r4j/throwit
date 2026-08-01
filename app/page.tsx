d57c1f88d7e7ce2dbc249ab96625089bf603745c
Sairaj Jawalikar <65067289+s41r4j@users.noreply.github.com>
2026-08-01 14:02:53 +0530

    Mount client-side Throwit enhancements

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Throwit from "@/components/Throwit";
+import ClientEnhancements from "@/components/ClientEnhancements";
 
 export default function Home() {
-  return <Throwit />;
+  return <><Throwit /><ClientEnhancements /></>;
 }
